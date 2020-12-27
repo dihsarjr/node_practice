5c0184c3b52f6536a818ff9049b779a69638fe57
dihsarjr <mohammed1rashid786@gmail.com>
2020-12-27 15:12:54 +0530

    test

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fs =require('fs');
+// javaScript play 
 
-fs.writeFileSync("hello.txt","hi hello World");
+// declace a veriable like in javaScript
+const  namer = "rashid";
+var age = 24;
+var areYouAProgramer= true;
+
+
+// function
+
+function user(names,age,programer){
+    return("name is " + names+" age is "+age+" are you a programer " + programer);
+
+}
+
+
+
+console.log(user(namer,age,areYouAProgramer));
