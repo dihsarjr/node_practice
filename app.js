bbadb0e5a2956e1ae1eeff45f777c76ef7252872
dihsarjr <mohammed1rashid786@gmail.com>
2020-12-28 22:45:46 +0530

    rou

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server =     http.createServer((request,response)=>{
+  var url =   request.url;
+  if(url === "/"){
+    response.write("<h1>hi this is ny first server 1 the main server..</h1>");
+  }else{
     response.write("<h1>hi this is ny first server..</h1>");
+  }
+ 
 });
 
 server.listen(3000);
